4a2691fe704ce8dfe9084320837e4f87bdb97462
Lalo <siedutunja@galeanoti.com>
2022-09-23 06:46:33 -0500

    Actualizacion 1-1-5

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,14 +1,14 @@
 //*** Api Localhost
-//export const ROOT_PATH = "http://localhost:5001/api/"
-//export const ROOT_HOME = "http://localhost:8080/login/"
-//export const ROOT_MODULO_ADMON = "http://localhost:8081/admon"
-//export const ROOT_MODULO_COLEGIO = "http://localhost:8082/colegio"
+export const ROOT_PATH = "http://localhost:5001/api/"
+export const ROOT_HOME = "http://localhost:8080/login/"
+export const ROOT_MODULO_ADMON = "http://localhost:8081/admon"
+export const ROOT_MODULO_COLEGIO = "http://localhost:8082/colegio"
 
 //*** Api Servidor
-export const ROOT_PATH = "https://siedutunja.gov.co:3001/api/"
-export const ROOT_HOME = "https://siedutunja.gov.co/login/"
-export const ROOT_MODULO_ADMON = "https://siedutunja.gov.co/admon"
-export const ROOT_MODULO_COLEGIO = "https://siedutunja.gov.co/colegio"
+//export const ROOT_PATH = "https://siedutunja.gov.co:3001/api/"
+//export const ROOT_HOME = "https://siedutunja.gov.co/login/"
+//export const ROOT_MODULO_ADMON = "https://siedutunja.gov.co/admon"
+//export const ROOT_MODULO_COLEGIO = "https://siedutunja.gov.co/colegio"
 
 export const ROOT_WEBSITE = "https://siedutunja.gov.co"
 
