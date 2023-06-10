26766c41f31b5e38efa680018c0c81836d2bea6d
Lalo <siedutunja@galeanoti.com>
2023-06-10 17:56:38 -0500

    Login Academico 1.1.1

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,19 +1,16 @@
 //*** Api Localhost
-export const ROOT_PATH = "http://localhost:5001/api/"
+export const ROOT_PATH = "http://localhost:5005/api/"
 export const ROOT_HOME = "http://localhost:8080/login/"
 export const ROOT_MODULO_ADMON = "http://localhost:8081/admon"
-export const ROOT_MODULO_COLEGIO = "http://localhost:8082/colegio"
+export const ROOT_MODULO_ESTUDIANTE = "http://localhost:8081/estudiante"
 
 //*** Api Servidor
-//export const ROOT_PATH = "https://siedutunja.gov.co:3001/api/"
-//export const ROOT_HOME = "https://siedutunja.gov.co/login/"
-//export const ROOT_MODULO_ADMON = "https://siedutunja.gov.co/admon"
-//export const ROOT_MODULO_COLEGIO = "https://siedutunja.gov.co/colegio"
+//export const ROOT_PATH = "https://sised.com.co:3002/api/"
+//export const ROOT_HOME = "https://sised.com.co/sisedplus/login/"
+//export const ROOT_MODULO_ADMON = "https://sised.com.co/sisedplus/admon"
+//export const ROOT_MODULO_ESTUDIANTE = "https://sised.com.co/sisedplus/estudiante"
 
-export const ROOT_WEBSITE = "https://siedutunja.gov.co"
-
-export const TITULO_MSG = "SieduTunja"
+export const TITULO_MSG = "SisedPlus"
 export const SECRET_KEY = "cataydany3"
-export const ALCALDIA = "Alcaldía Mayor de Tunja"
-export const SECRETARIA = "Secretaría de Educación de Tunja"
-export const PROYECTO = "SIEDUTUNJA | Gestión Administrativa Secretaría de Educación de Tunja"
+export const ROOT_ESCUDOS = "https://sised.com.co/sisedplus/escudos/"
+export const ROOT_WEBSITE = "https://sised.com.co"
